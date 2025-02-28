76d6dbfd9effc4d866913310884707a6728628f6
Karan-K5 <156467429+Karan-K5@users.noreply.github.com>
2025-02-28 06:04:17 -0500

    Update ReviewTester.java

--- AutoGenerateReview/ReviewTester.java
+++ AutoGenerateReview/ReviewTester.java
@@ -1,1 +1,8 @@
-
+public class ReviewTester
+{
+    public static void main(String[] args)
+    {
+        //Test your fakeReview method here!
+        System.out.println(Review.fakeReview("SimpleReview.txt"));
+    }
+}
